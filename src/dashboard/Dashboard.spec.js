67f4afeabd82c210fcda19c516f2e8d503bb7b2e
Vinni Hoke <vinni.hoke@gmail.com>
2019-10-02 20:18:18 -0700

    Some tests working

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,43 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Dashboard from "../dashboard/Dashboard";
+
+test("Dashboard renders", () => {
+  render(<Dashboard />);
+});
+
+describe("Dashboard Testing", () => {
+  test("Gate defaults to unlocked", () => {
+    const { getByText } = render(<Dashboard />);
+    expect(getByText(/unlocked/i));
+  });
+
+  test("Gate defaults to open", () => {
+    const { getByText } = render(<Dashboard />);
+    expect(getByText(/open/i));
+  });
+});
+
+describe("Async Controls Testing", () => {
+  test("Close toggles", async () => {
+    const { getByText, queryByText } = render(<Dashboard />);
+    expect(queryByText(/closed/i)).toBeNull();
+    await fireEvent.click(getByText(/close gate/i));
+    expect(getByText(/closed/i));
+  });
+
+  test("Close toggles", async () => {
+    const { getByText, queryByText } = render(<Dashboard />);
+    expect(queryByText(/open/i));
+    await fireEvent.click(getByText(/close gate/i));
+    expect(getByText(/closed/i));
+  });
+
+  test("Unlocked toggles", async () => {
+    const { getByText, queryByText } = render(<Dashboard />);
+    expect(queryByText(/unlocked/i));
+    await fireEvent.click(getByText(/lock gate/i));
+    expect(getByText(/locked/i));
+  });
+});
