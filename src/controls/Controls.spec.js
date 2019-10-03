5c61b18d5852f0b2717d2055a687717d5b42c8d5
Vinni Hoke <vinni.hoke@gmail.com>
2019-10-02 20:26:03 -0700

    Added control Spec

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+
+// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Controls from "./Controls";
+test("Render without crashing", () => {
+  render(<Controls />);
+});
+describe("Provide Buttons to Toggle Closed and Locked States", () => {
+  const { getByText } = render(<Controls />);
+  expect(getByText(/close gate/i));
+});
